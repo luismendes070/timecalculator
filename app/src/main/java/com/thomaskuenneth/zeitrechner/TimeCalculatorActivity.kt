f8e7ccacb8db853814bd3696209d0178734a261c
Luis Mendes <machadoluismendes@gmail.com>
2023-06-17 11:50:40 -0300

    Increment
    Screenshots chapter 2 HelloView Factorial

--- app/src/main/java/com/thomaskuenneth/zeitrechner/TimeCalculatorActivity.kt
+++ app/src/main/java/com/thomaskuenneth/zeitrechner/TimeCalculatorActivity.kt
@@ -1,6 +1,7 @@
 package com.thomaskuenneth.zeitrechner
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,15 +17,27 @@ class TimeCalculatorActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycleScope.launch {
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                setContent {
-                    MaterialTheme(
-                        content = { TimeCalculatorScreen() },
-                        colorScheme = defaultColorScheme()
-                    )
+
+        try{
+
+            lifecycleScope.launch {
+                lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    setContent {
+                        MaterialTheme(
+                            content = { TimeCalculatorScreen() },
+                            colorScheme = defaultColorScheme()
+                        )
+                    }
                 }
             }
+
+        }catch (e: Exception){
+
+            // Log.e("TAG", "Exception occurred, stack trace: " + e.getStackTraceString())
+
+        }finally {
+
         }
+
     }
 }
